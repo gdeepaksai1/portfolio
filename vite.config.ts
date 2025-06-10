44b9b69cdf36f27dd55885fa7cf9b655d73b5461
gdeepaksai1 <guntreddideepaksai@gmail.com>
2025-06-10 12:26:03 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/[YOUR_REPO_NAME]/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/deepak-sai-portfolio-nexus
+/' : '/',
   server: {
     host: "::",
     port: 8080,
